8322036adfbaded2cc9130dc0cde0b899a8349d6
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-06-30 21:50:14 +0200

    chore: meh

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -4,7 +4,8 @@ import { Recipe } from "./recipe";
 const recipe: Recipe = {
   flourAmount: 500,
   ingredients: [
-    { name: "White flour", percentage: 100, type: "flour" },
+    { name: "White flour", percentage: 90, type: "flour" },
+    { name: "Fullwheat flour", percentage: 10, type: "flour" },
     { name: "Water", percentage: 75, type: "water" },
     { name: "Salt", percentage: 2.2, type: "other" },
     { name: "Yeast", percentage: 2, type: "other" },
